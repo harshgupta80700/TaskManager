84673d4973da09e0299c2174e6c3ec3138f4d6c2
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-05 00:26:20 +0530

    promise function setup with error setup

--- playground/promises.js
+++ playground/promises.js
@@ -1,9 +1,12 @@
 const doWorkPromise = new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        resolve([1,2,3])
+        // resolve([1,2,3])
+        reject('Unable to connect')
     },2000)
 })
 
 doWorkPromise.then((result)=>{
     console.log('success',result)
+}).catch((error)=>{
+    console.log('Error',error)
 })
