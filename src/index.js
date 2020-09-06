8b6cb9955dac9a8b9e48cc8a0cb4629ddd93ea35
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-07 00:58:45 +0530

    task post reqs also done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/tasks')
+require('./models/tasks')
 
 const app = express()
 app.use(express.json())
@@ -14,12 +16,20 @@ app.post('/users',(req,res)=>{
 
     const user = new User(req.body)
     user.save().then(()=>{
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e)=>{
-        res.status(400)
-        res.send(e) 
+        res.status(400).send(e)
     })
 
+})
+
+app.post('/task',(req,res)=>{
+    const task = new Task(req.body)
+    task.save().then(()=>{
+        res.status(201).send(task)
+    }).catch((e)=>{
+        res.status(400).send(e)
+    })
 })
 
 
