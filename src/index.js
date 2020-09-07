61420715bbd5cccf235025523146e3e0a676287e
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-07 22:50:10 +0530

    task updates succesfully

--- src/index.js
+++ src/index.js
@@ -162,6 +162,31 @@ app.patch('/users/:id',async(req,res)=>{
     }
 })
 
+app.patch('/tasks/:id', async (req,res)=>{
+
+    const updates = Object.keys(req.body)
+    const validUpdates = ['description','completed']
+    const isValidUpdate = updates.every((value) => validUpdates.includes(value))
+
+    if(!isValidUpdate){
+        return res.status(400).send({
+            error: 'Not a Valid parameter passed'
+        })
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id,req.body,{new: true, runValidators: true})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.status(200).send(task)
+
+    }catch(e){
+        res.status(500).send(e)
+    }
+
+})
+
 app.listen(port,()=>{
     console.log('Server is on port',port)
 })
