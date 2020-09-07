2dbbad93401d36da5734280640ef189a5ed7d303
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-07 23:43:34 +0530

    users and tasks deleted method complete

--- src/index.js
+++ src/index.js
@@ -187,6 +187,34 @@ app.patch('/tasks/:id', async (req,res)=>{
 
 })
 
+app.delete('/users/:id', async (req,res)=>{
+
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user){
+            return res.status(404).send()
+        }
+        res.status(200).send(user)
+    }catch(e){
+        res.status(500).send(e)
+    } 
+
+})
+
+app.delete('/tasks/:id',async(req,res)=>{
+
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            return res.status(404).send()
+        }
+        res.status(200).send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
+
+})
+
 app.listen(port,()=>{
     console.log('Server is on port',port)
 })
