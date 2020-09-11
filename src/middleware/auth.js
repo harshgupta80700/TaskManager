f425ca1993319c70338df6efa9a007ac2dbf0e07
harshgupta80700 <harshgupta.80700@gmail.com>
2020-09-11 14:36:37 +0530

    token printed to the console

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,18 @@
+const jwt = require('jsonwebtoken');
+const jsw = require('express');
+
+
 const auth = (req,res,next)=>{
-    console.log("auth middleware")
-    next()
+    // console.log("auth middleware")
+    // next()
+
+    try{
+        const token = req.header('Authorization').replace('Bearer','')
+        console.log(token)
+    }catch(e){
+        res.status(401).send({error: 'please Authenticate'});
+    }
+
 }
 
 module.exports = auth
